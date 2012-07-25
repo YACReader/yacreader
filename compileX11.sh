5064165e9549eef634e7753e255c2ef74245a804
Luis Ángel San Martín <luisangelsm@gmail.com>
2012-07-26 00:17:39 +0200

    completado el script de compilaci?n y empaquetado en X11

--- compileX11.sh
+++ compileX11.sh
@@ -12,8 +12,20 @@ qmake
 make
 cd ..
 
-mkdir ./bin
-cp ./YACReader/YACReader ./bin/YACReader
-cp ./YACReaderLibrary/YACReaderLibrary ./bin/YACReaderLibrary
+echo "Copying to destination folder"
+dest='YACReader-'$1' X11-'$2'-qt4'
+mkdir "$dest"
+cp ./YACReader/YACReader "./${dest}/YACReader"
+cp ./YACReaderLibrary/YACReaderLibrary "./${dest}/YACReaderLibrary"
+cp ./COPYING.txt "./${dest}/"
+cp ./README.txt "./${dest}/"
+cp ./images/icon.png "./${dest}/"
+cp ./images/iconLibrary.png "./${dest}/"
+cp ./images/db.png "./${dest}/"
+cp ./images/coversPackage.png "./${dest}/"
+cp -R ./utils "./${dest}/"
+
+echo "Creating package"
+tar -czf "${dest}".tar.gz "${dest}"
 
 echo "Done!"
