04e7c51940d43832d6a95462112a01e336439c61
LuisAngelSM <luisangelsm@gmail.com>
2012-08-26 20:16:15 +0200

    modificado el script de mac os x para generar un .dmg

--- compileOSX.sh
+++ compileOSX.sh
@@ -12,11 +12,27 @@ qmake -spec macx-g++
 make
 cd ..
 
-mkdir ./bin
+echo "Configuring release apps"
+
 cp -R ./YACReader/YACReader.app ./YACReader.app
 cp -R ./YACReaderLibrary/YACReaderLibrary.app ./YACReaderLibrary.app
 
-echo "Configuring release apps"
 ./releaseOSX.sh
+
+
+echo "Copying to destination folder"
+dest='YACReader-'$1' MacOSX-Intel'
+mkdir "$dest"
+cp -R ./YACReader.app "./${dest}/YACReader.app"
+cp -R ./YACReaderLibrary.app "./${dest}/YACReaderLibrary.app"
+cp ./COPYING.txt "./${dest}/"
+cp ./README.txt "./${dest}/"
+
+cp ./images/db.png "./${dest}/"
+cp ./images/coversPackage.png "./${dest}/"
+
+echo "Creating dmg package"
+#tar -czf "${dest}".tar.gz "${dest}"
+hdiutil create "${dest}".dmg -srcfolder "./${dest}" -ov
 
 echo "Done!"
