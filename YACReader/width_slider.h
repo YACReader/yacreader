b6c49972f10f2eb0189c2e4e1e0efb079bc30b8a
Luis?ngel <Luis?ngel@Canquel-PC>
2013-02-08 15:12:55 +0100

    ficheros de c?digo fuente "width_slider" a?adidos al repositorio (se habian olvidado)

---
+++ YACReader/width_slider.h
@@ -0,0 +1,27 @@
+#ifndef WIDTH_SLIDER_H
+#define WIDTH_SLIDER_H
+
+#include <QWidgetAction>
+
+class QLabel;
+class QSlider;
+
+class YACReaderSliderAction : public QWidgetAction 
+{
+	Q_OBJECT
+private:
+	QLabel * percentageLabel;
+	QSlider * slider;
+public:
+	
+    YACReaderSliderAction (QWidget * parent = 0);
+
+public slots:
+	void updateText(int value);
+
+
+signals:
+	void fitToWidthRatioChanged(float value);
+};
+
+#endif
