38541f7c6ea8ceec598499593947f46ae4dc60e7
luisangelsm <luisangelsm@gmail.com>
2013-03-12 15:16:31 +0100

    corregido doble click en yacreader_flow_gl

--- common/yacreader_flow_gl.cpp
+++ common/yacreader_flow_gl.cpp
@@ -988,8 +988,31 @@ void YACReaderFlowGL::mousePressEvent(QMouseEvent *event)
 
 void YACReaderFlowGL::mouseDoubleClickEvent(QMouseEvent* event)
 {
-	emit selected(centerIndex());
-	event->accept();
+		float x,y;
+		x = event->x();
+		y = event->y();
+		GLint viewport[4];
+		GLdouble modelview[16];
+		GLdouble projection[16];
+		GLfloat winX, winY, winZ;
+		GLdouble posX, posY, posZ;
+
+		glGetDoublev( GL_MODELVIEW_MATRIX, modelview );
+		glGetDoublev( GL_PROJECTION_MATRIX, projection );
+		glGetIntegerv( GL_VIEWPORT, viewport );
+
+		winX = (float)x;
+		winY = (float)viewport[3] - (float)y;
+		glReadPixels( x, int(winY), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ );
+
+		gluUnProject( winX, winY, winZ, modelview, projection, viewport, &posX, &posY, &posZ);
+
+		if(posX <= 0.5 && posX >= -0.5)
+		{
+			emit selected(centerIndex());
+			event->accept();
+		}
+
 }
 
 YACReaderComicFlowGL::YACReaderComicFlowGL(QWidget *parent,struct Preset p )
