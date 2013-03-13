cf77e7ef394f79aef0032e2758b25cdc74878b53
Luis?ngel <Luis?ngel@Canquel-PC>
2013-03-13 21:43:33 +0100

    corregida la carga de lenguajes desde el directorio "./languages"

--- YACReaderLibrary/main.cpp
+++ YACReaderLibrary/main.cpp
@@ -13,7 +13,7 @@ int main( int argc, char ** argv )
 
   QTranslator translator;
   QString sufix = QLocale::system().name();
-  translator.load("./languages/yacreaderlibrary_"+sufix);
+  translator.load(QCoreApplication::applicationDirPath()+"/languages/yacreaderlibrary_"+sufix);
   app.installTranslator(&translator);
   app.setApplicationName("YACReaderLibrary");
 
