7bacad68840a73a0d19d704a75074a1fb007317f
Luis Ángel San Martín <luisangelsm@gmail.com>
2013-06-14 11:32:44 +0200

    despliegue de los lenguages disponibles

--- releaseOSX.sh
+++ releaseOSX.sh
@@ -4,6 +4,8 @@ macdeployqt YACReaderLibrary.app
 cp -R ./utils ./YACReader.app/Contents/MacOS/
 cp -R ./utils ./YACReaderLibrary.app/Contents/MacOS/
 cp -R ./release/server ./YACReaderLibrary.app/Contents/MacOS/
+cp -R ./release/languages ./YACReader.app/Contents/MacOS/
+cp -R ./release/languages ./YACReaderLibrary.app/Contents/MacOS/
 cd ./YACReaderLibrary.app/Contents/MacOS/
 ln -s ../../../YACReader.app/Contents/MacOS/YACReader
 
