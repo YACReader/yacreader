f6ff9751780bde1e2a43e07fd0ae62489ed9eaa7
luisangelsm <luisangelsm@localhost>
2013-12-22 05:11:18 -0800

    compilation fixed

--- YACReaderLibrary/library_window.cpp
+++ YACReaderLibrary/library_window.cpp
@@ -1053,7 +1053,7 @@ void LibraryWindow::openComic()
         yacreaderFound = QProcess::startDetached(QDir::cleanPath(QCoreApplication::applicationDirPath())+QString("/YACReader \"%1\" \"%2\" \"%3\"").arg(path).arg(comicId).arg(libraryId)/*.arg(page).arg(bookmark1).arg(bookmark2).arg(bookmark3).arg(brightness).arg(contrast).arg(gamma)*/,QStringList());
 #endif
 
-#if Q_OS_LINUX
+#ifdef Q_OS_LINUX
         QStringList parameters = QStringList() << path << QString::number(comicId) << QString::number(libraryId);
         yacreaderFound = QProcess::startDetached(QDir::cleanPath(QCoreApplication::applicationDirPath())+QString("/YACReader"),parameters);
 #endif
