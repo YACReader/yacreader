f84db8b4fc4abae4fc42a65fb8ed960548310870
Felix Kauselmann <2039670+selmf@users.noreply.github.com>
2014-08-24 11:44:00 +0200

    Create a yacreader-VERSION directory in the .tar.xz when using mktarball.sh

--- mktarball.sh
+++ mktarball.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
-
+#script to create a source tarball for YACReader distribution and packaging
 YACVERSION=7.2.0
 
 if [ ! -f "yacreader-${YACVERSION}-src.tar.xz" ]
 then
 	echo "Building source tarball"
-	tar cfJ yacreader-${YACVERSION}-src.tar.xz --exclude='.*' --exclude 'dependencies' --exclude 'yacreader*tar*' *
-	md5sum yacreader-${YACVERSION}-src.tar.xz > yacreader-${YACVERSION}-src.tar.xz.md5sum
 else
 	echo "Updating source tarball"
 	rm yacreader-${YACVERSION}-src.tar* #delete old tarball, since tar can't update compressed archives
-	tar cfJ yacreader-${YACVERSION}-src.tar.xz --exclude='.*' --exclude 'dependencies' --exclude 'yacreader*tar*' *
-	md5sum yacreader-${YACVERSION}-src.tar.xz > yacreader-${YACVERSION}-src.tar.xz.md5sum
 fi
+
+tar cfJ yacreader-${YACVERSION}-src.tar.xz --exclude-vcs --exclude-vcs-ignores --exclude 'dependencies' \
+--exclude 'yacreader*tar*' ./* --transform s/./yacreader-${YACVERSION}/
+md5sum yacreader-${YACVERSION}-src.tar.xz > yacreader-${YACVERSION}-src.tar.xz.md5sum
