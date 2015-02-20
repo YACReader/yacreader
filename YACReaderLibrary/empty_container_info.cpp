fb13bb1e5ff671436fce021402bc885589194652
Felix Kauselmann <2039670+selmf@users.noreply.github.com>
2015-02-20 01:06:07 +0100

    Fix racing condition in comic.cpp pdf render code (issue 25)

---
+++ YACReaderLibrary/empty_container_info.cpp
@@ -0,0 +1,47 @@
+#include "empty_container_info.h"
+
+EmptyContainerInfo::EmptyContainerInfo(QWidget *parent) :
+    QWidget(parent), iconLabel(new QLabel()), titleLabel(new QLabel())
+{
+#ifdef Q_OS_MAC
+    backgroundColor = "#FFFFFF";
+    titleLabel->setStyleSheet("QLabel {color:#888888; font-size:24px;font-family:Arial;font-weight:bold;}");
+#else
+    backgroundColor = "#2A2A2A";
+    titleLabel->setStyleSheet("QLabel {color:#CCCCCC; font-size:24px;font-family:Arial;font-weight:bold;}");
+#endif
+
+    iconLabel->setAlignment(Qt::AlignCenter);
+    titleLabel->setAlignment(Qt::AlignCenter);
+}
+
+void EmptyContainerInfo::setPixmap(const QPixmap &pixmap)
+{
+    iconLabel->setPixmap(pixmap);
+}
+
+void EmptyContainerInfo::setText(const QString &text)
+{
+    titleLabel->setText(text);
+}
+
+QVBoxLayout * EmptyContainerInfo::setUpDefaultLayout(bool addStretch)
+{
+    QVBoxLayout * layout = new QVBoxLayout;
+
+    layout->addSpacing(100);
+    layout->addWidget(iconLabel);
+    layout->addSpacing(30);
+    layout->addWidget(titleLabel);
+    if(addStretch)
+        layout->addStretch();
+
+    setLayout(layout);
+    return layout;
+}
+
+void EmptyContainerInfo::paintEvent(QPaintEvent *)
+{
+    QPainter painter (this);
+    painter.fillRect(0,0,width(),height(),QColor(backgroundColor));
+}
---
+++ custom_widgets/yacreader_dark_menu.h
@@ -0,0 +1,14 @@
+#ifndef YACREADER_DARK_MENU_H
+#define YACREADER_DARK_MENU_H
+
+#include <QMenu>
+
+
+class YACReaderDarkMenu : public QMenu
+{
+  Q_OBJECT
+	public:
+		YACReaderDarkMenu(QWidget * parent = 0);
+};
+
+#endif // YACREADER_DARK_MENU_H
