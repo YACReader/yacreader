c77ef524de75f6ae084c956ae0f50a7a5a9b6f7c
Luis Ángel San Martín <luisangelsm@gmail.com>
2015-03-18 16:03:00 +0100

    new ip sorting heuristic in server dialog

---
+++ YACReaderLibrary/yacreader_local_server.h
@@ -0,0 +1,50 @@
+#ifndef YACREADER_LOCAL_SERVER_H
+#define YACREADER_LOCAL_SERVER_H
+
+#include <QObject>
+#include <QThread>
+#include <QMutex>
+
+class QLocalServer;
+class QLocalSocket;
+class ComicDB;
+
+class YACReaderLocalServer : public QObject
+{
+	Q_OBJECT
+public:
+	explicit YACReaderLocalServer(QObject *parent = 0);
+	
+signals:
+	void comicUpdated(quint64 libraryId, const ComicDB & comic);
+public slots:
+	bool isListening();
+	void sendResponse();
+	static bool isRunning();
+    void close();
+private:
+	//void run();
+	QLocalServer * localServer;
+	
+};
+
+class YACReaderClientConnectionWorker : public QThread
+{
+	Q_OBJECT
+public:
+	YACReaderClientConnectionWorker( QLocalSocket *clientConnection);
+	~YACReaderClientConnectionWorker();
+signals:
+	void comicUpdated(quint64 libraryId, const ComicDB & comic);
+private:
+	static QMutex dbMutex;
+	//static int count;
+	void run();
+	
+	void getComicInfo(quint64 libraryId, ComicDB & comic, QList<ComicDB> & sibling);
+	void updateComic(quint64 libraryId, ComicDB & comic);
+
+	QLocalSocket *clientConnection;
+};
+
+#endif // YACREADER_LOCAL_SERVER_H
---
+++ custom_widgets/yacreader_deleting_progress.h
@@ -0,0 +1,26 @@
+#ifndef YACREADER_DELETING_PROGRESS_H
+#define YACREADER_DELETING_PROGRESS_H
+
+#include <QWidget>
+
+class QLabel;
+
+class YACReaderDeletingProgress : public QWidget
+{
+	Q_OBJECT
+public:
+	explicit YACReaderDeletingProgress(QWidget *parent = 0);
+	QSize sizeHint() const;
+signals:
+	
+public slots:
+	
+protected:
+	void paintEvent(QPaintEvent *);
+
+private:
+	QLabel * textMessage;
+	
+};
+
+#endif // YACREADER_DELETING_PROGRESS_H
