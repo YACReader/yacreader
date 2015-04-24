30fda1a6f2783c6a5c178d607c1ad3ff480d2542
Luis ?ngel San Mart?n Rodr?guez <luisangelsm@gmail.com>
2015-04-24 22:48:17 +0200

    added sync back reading progress from remote clients - full info sync is out of scope by now

--- YACReaderLibrary/server/controllers/synccontroller.cpp
+++ YACReaderLibrary/server/controllers/synccontroller.cpp
@@ -3,6 +3,9 @@
 #include "QsLog.h"
 #include <QUrl>
 
+#include "comic_db.h"
+#include "db_helper.h"
+
 SyncController::SyncController()
 {
 
@@ -10,15 +13,42 @@ SyncController::SyncController()
 
 void SyncController::service(HttpRequest &request, HttpResponse &response)
 {
-    QString path = QUrl::fromPercentEncoding(request.getPath()).toUtf8();
-    QStringList pathElements = path.split('/');
-    qulonglong libraryId = pathElements.at(2).toULongLong();
-
     QString postData = QString::fromUtf8(request.getBody());
 
     QLOG_INFO() << "POST DATA: " << postData;
 
-    //TODO Process postData and update the comics
+    if(postData.length()>0) {
+        QList<QString> data = postData.split("\n");
+
+        qulonglong libraryId;
+        qulonglong comicId;
+        int currentPage;
+        QString hash;
+        foreach(QString comicInfo, data)
+        {
+            QList<QString> comicInfoProgress = comicInfo.split("\t");
+
+            if(comicInfoProgress.length() == 4)
+            {
+                libraryId = comicInfoProgress.at(0).toULongLong();
+                comicId = comicInfoProgress.at(1).toULongLong();
+                hash = comicInfoProgress.at(2);
+                currentPage = comicInfoProgress.at(3).toInt();
+
+                ComicInfo info;
+                info.currentPage = currentPage;
+                info.hash = hash; //TODO remove the hash check and add UUIDs for libraries
+                info.id = comicId;
+                DBHelper::updateFromRemoteClient(libraryId,info);
+            }
+        }
+    }
+    else
+    {
+        response.setStatus(412,"No comic info received");
+        response.writeText("",true);
+        return;
+    }
 
     response.write("OK",true);
 }
