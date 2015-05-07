12b123537d83ad0957577e66879c06671acc65aa
Luis Ángel San Martín <luisangelsm@gmail.com>
2015-05-07 22:01:55 +0200

    added missing files

---
+++ common/opengl_checker.h
@@ -0,0 +1,15 @@
+#ifndef OPENGL_CHECKER_H
+#define OPENGL_CHECKER_H
+
+#include <QOpenGLContext>
+
+class OpenGLChecker
+{
+public:
+    OpenGLChecker();
+    bool hasCompatibleOpenGLVersion();
+private:
+    //??
+};
+
+#endif // OPENGL_CHECKER_H
