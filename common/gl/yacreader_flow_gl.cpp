e6064b912691d67b35547e5a7f74bf962630a4ac
Luis Ángel San Martín <luisangelsm@gmail.com>
2015-11-14 17:16:47 +0100

    Added call to missing glClearColor method in flow classes, this was causing render issues in some platforms.

--- common/gl/yacreader_flow_gl.cpp
+++ common/gl/yacreader_flow_gl.cpp
@@ -325,6 +325,7 @@ void YACReaderFlowGL::paintGL()
 
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+    glClearColor(0,0,0,1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     if(numObjects>0)
