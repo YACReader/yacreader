7b3de7df54aa14d3746d503ea69282fc6842a39d
selmf <licorn@gmail.com>
2015-12-31 18:36:12 +0100

    Add virtual destructor to ImageFilter class to remove compiler warning for undefined behavior

--- YACReader/render.h
+++ YACReader/render.h
@@ -21,6 +21,8 @@ class Render;
 class ImageFilter {
 public:
 	ImageFilter(){};
+	//TODO: Ask Luis if this is a good idea
+	virtual ~ImageFilter() {};
 	virtual QImage setFilter(const QImage & image) = 0;
 	inline int getLevel() {return level;};
 	inline void setLevel(int l) {level = l;};
