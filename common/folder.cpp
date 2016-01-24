baef22a054f7b2a16381ad61b65e73b9119413be
Luis ?ngel San Mart?n Rodr?guez <luisangelsm@gmail.com>
2016-01-24 19:14:16 +0100

    store the hash of the first comic in a folder, this is more useful than storing the id

--- common/folder.cpp
+++ common/folder.cpp
@@ -3,15 +3,13 @@
 Folder::Folder()
     :knownParent(false),
      knownId(false),
-     numChildren(-1),
-     firstChildId(0)
+     numChildren(-1)
 {}
 
 Folder::Folder(qulonglong folderId, qulonglong parentId, const QString &folderName, const QString &folderPath)
     :knownParent(true),
      knownId(true),
-     numChildren(-1),
-     firstChildId(0)
+     numChildren(-1)
 {
      this->id = folderId;
      this->parentId = parentId;
@@ -22,8 +20,7 @@ Folder::Folder(qulonglong folderId, qulonglong parentId, const QString &folderNa
 Folder::Folder(const QString & folderName, const QString & folderPath)
     :knownParent(false),
      knownId(false),
-     numChildren(-1),
-     firstChildId(0)
+     numChildren(-1)
 {
     this->name = folderName;
     this->path = folderPath;
