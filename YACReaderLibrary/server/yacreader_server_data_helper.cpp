2aa5c00d3452280e1171b54f09e33a8c987ddd69
Luis Ángel San Martín <luisangelsm@gmail.com>
2016-08-23 18:03:31 +0200

    Include missing info in YSFormat for comics.

--- YACReaderLibrary/server/yacreader_server_data_helper.cpp
+++ YACReaderLibrary/server/yacreader_server_data_helper.cpp
@@ -12,12 +12,15 @@ QString YACReaderServerDataHelper::folderToYSFormat(const qulonglong libraryId, 
 
 QString YACReaderServerDataHelper::comicToYSFormat(const qulonglong libraryId,const ComicDB & comic)
 {
-    return QString("c\t%1\t%2\t%3\t%4\t%5\r\n")
+    return QString("c\t%1\t%2\t%3\t%4\t%5\t%6\t%7\t%8\r\n")
             .arg(libraryId)
             .arg(comic.id)
             .arg(comic.getFileName())
             .arg(comic.getFileSize())
-            .arg(comic.info.hash);
+            .arg(comic.info.hash)
+            .arg(comic.info.currentPage)
+            .arg(comic.info.numPages.toInt())
+            .arg(comic.info.read?1:0);
 }
 
 YACReaderServerDataHelper::YACReaderServerDataHelper() {}
