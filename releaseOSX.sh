987103aaca6b5d63ee237d6099d393872fa84da9
Felix Kauselmann <licorn@gmail.com>
2017-04-16 12:41:06 +0200

    More mac compile script simplification

--- releaseOSX.sh
+++ releaseOSX.sh
@@ -6,9 +6,11 @@ macdeployqt YACReader.app
 macdeployqt YACReaderLibrary.app -qmldir=YACReaderLibrary/qml
 macdeployqt YACReaderLibraryServer.app
 
+#TODO: This copies unneeded stuff into the respective apps
 cp -R utils YACReader.app/Contents/MacOS/
 cp -R utils YACReaderLibrary.app/Contents/MacOS/
 cp -R utils YACReaderLibraryServer.app/Contents/MacOS/
+
 cp -R release/server YACReaderLibrary.app/Contents/MacOS/
 cp -R release/server YACReaderLibraryServer.app/Contents/MacOS/
 cp -R release/languages YACReader.app/Contents/MacOS/
