ffaefefc6329cd39a5875b663060d1512512ebf3
selmf <licorn@gmail.com>
2017-09-24 21:15:16 +0200

    Use hg archive to create source tarball

--- mktarball.sh
+++ mktarball.sh
@@ -2,11 +2,7 @@
 #Script to create a source tarball for YACReader distribution and packaging
 #This should be run from YACReaders top source directory
 
-YACVERSION=8.6
-if [ -f Makefile ]
-then
-	make distclean
-fi
+YACVERSION=9.0-dev
 if [ ! -f "yacreader-${YACVERSION}-src.tar.xz" ]
 then
 	echo "Building source tarball"
@@ -14,9 +10,11 @@ else
 	echo "Updating source tarball"
 	rm yacreader-${YACVERSION}-src.tar* #delete old tarball, since tar can't update compressed archives
 fi
-#Use tar's --exclude feature to make sure we get a pristine tar for distribution.
-#Exclude all version control system related files and rename the top directory in the tarball using --transform.
-tar cfJ yacreader-${YACVERSION}-src.tar.xz  --exclude '*.rej' --exclude '*.orig' --exclude '*.gch' --exclude 'dependencies' --exclude '*.o' \
---exclude 'yacreader*tar*' --exclude '.hg*' --exclude 'lib7zip' --exclude 'libp7zip' --exclude 'unarr-master' --exclude-vcs ./* --transform s/./yacreader-${YACVERSION}/
-#Calculate checksum to enable packagers to verify whether they are using the original tarball. 
+
+hg archive -t tar -p yacreader-${YACVERSION} -X "dependencies/{unarr,pdfium,poppler}"\
+  yacreader-${YACVERSION}-src.tar
+xz -c yacreader-${YACVERSION}-src.tar > yacreader-${YACVERSION}-src.tar.xz
+rm yacreader-${YACVERSION}-src.tar
+
+#Calculate checksum to enable packagers to verify whether they are using the original tarball.
 md5sum yacreader-${YACVERSION}-src.tar.xz > yacreader-${YACVERSION}-src.tar.xz.md5sum
