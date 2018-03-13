cd87622f13649642638c639ab89b2a50c0e8ebe2
Luis ?ngel San Mart?n Rodr?guez <luisangelsm@gmail.com>
2018-03-13 19:08:39 +0100

    Send the cover size ratio in the comic info json.

--- YACReaderLibrary/server/yacreader_server_data_helper.cpp
+++ YACReaderLibrary/server/yacreader_server_data_helper.cpp
@@ -35,6 +35,7 @@ QJsonObject YACReaderServerDataHelper::comicToJSON(const qulonglong libraryId, c
     json["current_page"] = comic.info.currentPage;
     json["num_pages"] = comic.info.numPages.toInt();
     json["read"] = comic.info.read;
+    json["cover_size_ratio"] = comic.info.coverSizeRatio.toFloat();
 
     return json;
 }
