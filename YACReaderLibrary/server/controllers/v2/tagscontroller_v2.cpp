8f73547b4752af89eb7b0fe62e82f10d467b12dc
Luis ?ngel San Mart?n Rodr?guez <luisangelsm@gmail.com>
2018-04-24 14:42:30 +0200

    Migrate some more controllers to json.

--- YACReaderLibrary/server/controllers/v2/tagscontroller_v2.cpp
+++ YACReaderLibrary/server/controllers/v2/tagscontroller_v2.cpp
@@ -7,6 +7,8 @@
 #include "../static.h"
 #include "yacreader_global.h"
 
+#include "yacreader_server_data_helper.h"
+
 #include "QsLog.h"
 
 TagsControllerV2::TagsControllerV2() {}
@@ -19,12 +21,16 @@ void TagsControllerV2::service(HttpRequest& request, HttpResponse& response)
     QStringList pathElements = path.split('/');
     int libraryId = pathElements.at(3).toInt();
 
-    QList<Label> tags = DBHelper::getLabels(libraryId);
+    QList<Label> labels = DBHelper::getLabels(libraryId);
 
-    foreach(const Label &tag, tags)
+    QJsonArray items;
+
+    for(QList<Label>::const_iterator itr = labels.constBegin();itr!=labels.constEnd();itr++)
     {
-        response.write(QString("%1\t%2\t%3\t%4\r\n").arg(libraryId).arg(tag.getId()).arg(tag.getName()).arg(labelColorToRGBString(tag.getColorID())).toUtf8());
+        items.append(YACReaderServerDataHelper::labelToJSON(libraryId, *itr));
     }
 
-    response.write("",true);
+    QJsonDocument output(items);
+
+    response.write(output.toJson());;
 }
