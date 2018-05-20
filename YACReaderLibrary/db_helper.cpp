45e44fa54f9bfdf8093cc807289d07b14ad5ce4d
Luis Ángel San Martín <luisangelsm@gmail.com>
2018-05-20 14:48:08 +0200

    Only set a comic as reading if it has been opened beyod the cover.

--- YACReaderLibrary/db_helper.cpp
+++ YACReaderLibrary/db_helper.cpp
@@ -695,7 +695,7 @@ void DBHelper::updateProgress(qulonglong libraryId, const ComicInfo &comicInfo)
 
     ComicDB comic = DBHelper::loadComic(comicInfo.id,db);
     comic.info.currentPage = comicInfo.currentPage;
-    comic.info.hasBeenOpened = true;
+    comic.info.hasBeenOpened = comicInfo.currentPage > 0 || comic.info.hasBeenOpened;
     comic.info.read = comic.info.read || comic.info.currentPage == comic.info.numPages;
 
     DBHelper::updateReadingRemoteProgress(comic.info,db);
