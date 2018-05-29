dba264c31fb0e786f46033dcc6fbedf7feeb7fe1
Luis Ángel San Martín <luisangelsm@gmail.com>
2018-05-29 17:12:34 +0200

    Remove old commented code.

--- YACReaderLibrary/server/controllers/v2/covercontroller_v2.cpp
+++ YACReaderLibrary/server/controllers/v2/covercontroller_v2.cpp
@@ -11,7 +11,6 @@ CoverControllerV2::CoverControllerV2() {}
 void CoverControllerV2::service(HttpRequest& request, HttpResponse& response)
 {
 	response.setHeader("Content-Type", "image/jpeg");
-	//response.setHeader("Content-Type", "plain/text; charset=ISO-8859-1");
 
 	YACReaderLibraries libraries = DBHelper::getLibraries();
 
@@ -20,60 +19,14 @@ void CoverControllerV2::service(HttpRequest& request, HttpResponse& response)
     QString libraryName = DBHelper::getLibraryName(pathElements.at(3).toInt());
     QString fileName = pathElements.at(5);
 
-	//response.writeText(path+"<br/>");
-	//response.writeText(libraryName+"<br/>");
-	//response.writeText(libraries.value(libraryName)+"/.yacreaderlibrary/covers/"+fileName+"<br/>");
-
-	//QFile file(libraries.value(libraryName)+"/.yacreaderlibrary/covers/"+fileName);
-	//if (file.exists()) {
-	//	if (file.open(QIODevice::ReadOnly))
-	//	{
-	//		qDebug("StaticFileController: Open file %s",qPrintable(file.fileName()));
-	//		// Return the file content, do not store in cache
-	//		while (!file.atEnd() && !file.error()) {
-	//			response.write(file.read(131072));
-	//		}
-	//	}
-
-	//	file.close();
-	//}
-
 	QImage img(libraries.getPath(libraryName)+"/.yacreaderlibrary/covers/"+fileName);
 	if (!img.isNull()) {
-
-        /*int width = 80, height = 120;
-        if(ySession->getDisplayType()=="@2x")
-        {
-			width = 160;
-            height = 240;
-        }
-
-        if(float(img.width())/img.height() < 0.66666)
-            img = img.scaledToWidth(width,Qt::SmoothTransformation);
-        else
-            img = img.scaledToHeight(height,Qt::SmoothTransformation);
-
-        QImage destImg(width,height,QImage::Format_RGB32);
-        destImg.fill(Qt::black);
-        QPainter p(&destImg);
-
-        p.drawImage((width-img.width())/2,(height-img.height())/2,img);
-
-        if(folderCover)
-        {
-             if(ySession->getDisplayType()=="@2x")
-                p.drawImage(0,0,QImage(":/images/f_overlayed_retina.png"));
-             else
-                p.drawImage(0,0,QImage(":/images/f_overlayed.png"));
-        }
-*/
 		QByteArray ba;
 		QBuffer buffer(&ba);
 		buffer.open(QIODevice::WriteOnly);
         img.save(&buffer, "JPG");
 		response.write(ba,true);
 	}
-	//DONE else, hay que devolver un 404
 	else
 	{
 		response.setStatus(404,"not found");
