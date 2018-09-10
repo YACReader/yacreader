8a003dbfa8c755779d5c1e758a736fd771936058
Luis Ángel San Martín <luisangelsm@gmail.com>
2018-09-10 11:20:27 +0200

    Add new them properties to store specific styles for library items.

--- YACReaderLibrary/theme.h
+++ YACReaderLibrary/theme.h
@@ -17,12 +17,18 @@ public:
             t.disableClassicViewCollapsing = true;
 
             t.comicsViewTransitionBackground = "#FFFFFF";
+
+            t.itemLibrarySelectedStyleSheet = "color: white; background-color:#91c4f4; border-bottom:1px solid #91c4f4;";
+            t.itemLibraryNoSelectedStyleSheet = "background-color:transparent;";
         } else {
             t.isMacosNative = false;
 
             t.disableClassicViewCollapsing = false;
 
             t.comicsViewTransitionBackground = "#2A2A2A";
+
+            t.itemLibrarySelectedStyleSheet = "color: white; background-color:#2E2E2E; font-weight:bold;";
+            t.itemLibraryNoSelectedStyleSheet = "background-color:transparent; color:#DDDFDF;";
         }
 
         return t;
@@ -35,6 +41,10 @@ public:
 
     //
     QString comicsViewTransitionBackground;
+
+    //library item
+    QString itemLibrarySelectedStyleSheet;
+    QString itemLibraryNoSelectedStyleSheet;
 };
 
 #endif // THEME_H
