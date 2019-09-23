ae37010ef0d85f0fd6f0c3077daecfc49dfc4c40
Luis Ángel San Martín <luisangelsm@gmail.com>
2019-09-23 18:32:25 +0200

    Fix scopes

--- YACReaderLibrary/db/folder_model.cpp
+++ YACReaderLibrary/db/folder_model.cpp
@@ -694,119 +694,116 @@ void FolderModelProxy::setupFilteredModelData()
                     queryString += " AND f.id <> 1 ORDER BY f.parentId,f.name";
                     QLOG_ERROR() << "not implemented";
                     break;
-
-                    selectQuery.prepare(QString(queryString.c_str()));
-                    result.bindValues(selectQuery);
-
-                    selectQuery.prepare(queryString.c_str());
-                    result.bindValues(selectQuery);
                 }
-                catch (const std::exception &e)
-                {
-                    QLOG_ERROR() << "Unable to parse query: " << e.what();
-                }
-                selectQuery.exec();
-                QLOG_DEBUG() << selectQuery.lastError() << "--";
 
-                setupFilteredModelData(selectQuery, rootItem);
+                selectQuery.prepare(QString(queryString.c_str()));
+                result.bindValues(selectQuery);
+            } catch (const std::exception &e) {
+                QLOG_ERROR() << "Unable to parse query: " << e.what();
             }
-            //selectQuery.finish();
-            db.close();
-            QSqlDatabase::removeDatabase(db.connectionName());
+            selectQuery.exec();
+            QLOG_DEBUG() << selectQuery.lastError() << "--";
 
-            endResetModel();
+            setupFilteredModelData(selectQuery, rootItem);
         }
+        //selectQuery.finish();
+        db.close();
+        QSqlDatabase::removeDatabase(db.connectionName());
 
-        void FolderModelProxy::clear()
+        endResetModel();
+    }
+}
+
+void FolderModelProxy::clear()
+{
+    filterEnabled = false;
+
+    filteredItems.clear();
+
+    QSortFilterProxyModel::clear();
+}
+
+void FolderModelProxy::setupFilteredModelData(QSqlQuery &sqlquery, FolderItem *parent)
+{
+    auto model = static_cast<FolderModel *>(sourceModel());
+
+    //64 bits para la primary key, es decir la misma precisi�n que soporta sqlit 2^64
+    filteredItems.clear();
+
+    //se a�ade el nodo 0 al modelo que representa el arbol de elementos que cumplen con el filtro
+    filteredItems.insert(parent->id, parent);
+
+    QSqlRecord record = sqlquery.record();
+
+    int name = record.indexOf("name");
+    int path = record.indexOf("path");
+    int finished = record.indexOf("finished");
+    int completed = record.indexOf("completed");
+    int parentIdIndex = record.indexOf("parentId");
+
+    while (sqlquery.next()) { //se procesan todos los folders que cumplen con el filtro
+        //datos de la base de datos
+        QList<QVariant> data;
+
+        data << sqlquery.value(name).toString();
+        data << sqlquery.value(path).toString();
+        data << sqlquery.value(finished).toBool();
+        data << sqlquery.value(completed).toBool();
+
+        auto item = new FolderItem(data);
+        item->id = sqlquery.value(0).toULongLong();
+
+        //id del padre
+        quint64 parentId = sqlquery.value(parentIdIndex).toULongLong();
+
+        //se a�ade el item al map, de forma que se pueda encontrar como padre en siguientes iteraciones
+        if (!filteredItems.contains(item->id))
+            filteredItems.insert(item->id, item);
+
+        //es necesario conocer las coordenadas de origen para poder realizar scroll autom�tico en la vista
+        item->originalItem = model->items.value(item->id);
+
+        //si el padre ya existe en el modelo, el item se a�ade como hijo
+        if (filteredItems.contains(parentId))
+            filteredItems.value(parentId)->appendChild(item);
+        else //si el padre a�n no se ha a�adido, hay que a�adirlo a �l y todos los padres hasta el nodo ra�z
         {
-            filterEnabled = false;
+            //comprobamos con esta variable si el �ltimo de los padres (antes del nodo ra�z) ya exist�a en el modelo
+            bool parentPreviousInserted = false;
 
-            filteredItems.clear();
+            //mientras no se alcance el nodo ra�z se procesan todos los padres (de abajo a arriba)
+            while (parentId != ROOT) {
+                //el padre no estaba en el modelo filtrado, as� que se rescata del modelo original
+                FolderItem *parentItem = model->items.value(parentId);
+                //se debe crear un nuevo nodo (para no compartir los hijos con el nodo original)
+                FolderItem *newparentItem = new FolderItem(parentItem->getData()); //padre que se a�adir� a la estructura de directorios filtrados
+                newparentItem->id = parentId;
 
-            QSortFilterProxyModel::clear();
-        }
+                newparentItem->originalItem = parentItem;
 
-        void FolderModelProxy::setupFilteredModelData(QSqlQuery & sqlquery, FolderItem * parent)
-        {
-            auto model = static_cast<FolderModel *>(sourceModel());
-
-            //64 bits para la primary key, es decir la misma precisi�n que soporta sqlit 2^64
-            filteredItems.clear();
-
-            //se a�ade el nodo 0 al modelo que representa el arbol de elementos que cumplen con el filtro
-            filteredItems.insert(parent->id, parent);
-
-            QSqlRecord record = sqlquery.record();
-
-            int name = record.indexOf("name");
-            int path = record.indexOf("path");
-            int finished = record.indexOf("finished");
-            int completed = record.indexOf("completed");
-            int parentIdIndex = record.indexOf("parentId");
-
-            while (sqlquery.next()) { //se procesan todos los folders que cumplen con el filtro
-                //datos de la base de datos
-                QList<QVariant> data;
-
-                data << sqlquery.value(name).toString();
-                data << sqlquery.value(path).toString();
-                data << sqlquery.value(finished).toBool();
-                data << sqlquery.value(completed).toBool();
-
-                auto item = new FolderItem(data);
-                item->id = sqlquery.value(0).toULongLong();
-
-                //id del padre
-                quint64 parentId = sqlquery.value(parentIdIndex).toULongLong();
-
-                //se a�ade el item al map, de forma que se pueda encontrar como padre en siguientes iteraciones
-                if (!filteredItems.contains(item->id))
-                    filteredItems.insert(item->id, item);
-
-                //es necesario conocer las coordenadas de origen para poder realizar scroll autom�tico en la vista
-                item->originalItem = model->items.value(item->id);
-
-                //si el padre ya existe en el modelo, el item se a�ade como hijo
-                if (filteredItems.contains(parentId))
+                //si el modelo contiene al padre, se a�ade el item actual como hijo
+                if (filteredItems.contains(parentId)) {
                     filteredItems.value(parentId)->appendChild(item);
-                else //si el padre a�n no se ha a�adido, hay que a�adirlo a �l y todos los padres hasta el nodo ra�z
-                {
-                    //comprobamos con esta variable si el �ltimo de los padres (antes del nodo ra�z) ya exist�a en el modelo
-                    bool parentPreviousInserted = false;
-
-                    //mientras no se alcance el nodo ra�z se procesan todos los padres (de abajo a arriba)
-                    while (parentId != ROOT) {
-                        //el padre no estaba en el modelo filtrado, as� que se rescata del modelo original
-                        FolderItem *parentItem = model->items.value(parentId);
-                        //se debe crear un nuevo nodo (para no compartir los hijos con el nodo original)
-                        FolderItem *newparentItem = new FolderItem(parentItem->getData()); //padre que se a�adir� a la estructura de directorios filtrados
-                        newparentItem->id = parentId;
-
-                        newparentItem->originalItem = parentItem;
-
-                        //si el modelo contiene al padre, se a�ade el item actual como hijo
-                        if (filteredItems.contains(parentId)) {
-                            filteredItems.value(parentId)->appendChild(item);
-                            parentPreviousInserted = true;
-                        }
-                        //sino se registra el nodo para poder encontrarlo con posterioridad y se a�ade el item actual como hijo
-                        else {
-                            newparentItem->appendChild(item);
-                            filteredItems.insert(newparentItem->id, newparentItem);
-                            parentPreviousInserted = false;
-                        }
-
-                        //variables de control del bucle, se avanza hacia el nodo padre
-                        item = newparentItem;
-                        parentId = parentItem->parentItem->id;
-                    }
-
-                    //si el nodo es hijo de 1 y no hab�a sido previamente insertado como hijo, se a�ade como tal
-                    if (!parentPreviousInserted) {
-                        filteredItems.value(ROOT)->appendChild(item);
-                    } else {
-                        delete item;
-                    }
+                    parentPreviousInserted = true;
                 }
+                //sino se registra el nodo para poder encontrarlo con posterioridad y se a�ade el item actual como hijo
+                else {
+                    newparentItem->appendChild(item);
+                    filteredItems.insert(newparentItem->id, newparentItem);
+                    parentPreviousInserted = false;
+                }
+
+                //variables de control del bucle, se avanza hacia el nodo padre
+                item = newparentItem;
+                parentId = parentItem->parentItem->id;
+            }
+
+            //si el nodo es hijo de 1 y no hab�a sido previamente insertado como hijo, se a�ade como tal
+            if (!parentPreviousInserted) {
+                filteredItems.value(ROOT)->appendChild(item);
+            } else {
+                delete item;
             }
         }
+    }
+}
