f70799502ba687a53b7e1a51839b2497e57fc30f
Luis Ángel San Martín <luisangelsm@gmail.com>
2022-05-28 11:38:58 +0200

    Bump version number

--- common/yacreader_global.h
+++ common/yacreader_global.h
@@ -5,7 +5,7 @@
 #include <QDataStream>
 #include <QMetaType>
 
-#define VERSION "9.8.2"
+#define VERSION "9.9.0"
 
 #define REMOTE_BROWSE_PERFORMANCE_WORKAROUND "REMOTE_BROWSE_PERFORMANCE_WORKAROUND"
 
