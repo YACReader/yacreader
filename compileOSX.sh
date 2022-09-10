d5dc5fbecf006d5547c99c00b4749a0b28321136
Luis Ángel San Martín Rodríguez <luisangelsm@gmail.com>
2022-09-10 19:25:47 +0200

    Update dmg.json to include the Qt flavor

--- compileOSX.sh
+++ compileOSX.sh
@@ -9,7 +9,9 @@ SKIP_CODESIGN=${3:-false}
 
 QT_VERSION=${4:-""}
 
-if [ "$4" == "clean" ]; then
+echo "building macos binaries with these params: ${VERSION} ${BUILD_NUMBER} ${SKIP_CODESIGN} ${QT_VERSION}"
+
+if [ "$5" == "clean" ]; then
 ./cleanOSX.sh
 fi
 
@@ -75,8 +77,8 @@ fi
 
 echo "Preparing apps for release, Done."
 
-echo "Copying to destination folder"
-dest="YACReader-$VERSION.$BUILD_NUMBER MacOSX-Intel$QT_VERSION"
+dest="YACReader-$VERSION.$BUILD_NUMBER MacOSX-Intel ${QT_VERSION}"
+echo "Copying to destination folder ${dest}"
 mkdir -p "$dest"
 cp -R YACReader.app "${dest}/YACReader.app"
 cp -R YACReaderLibrary.app "${dest}/YACReaderLibrary.app"
@@ -85,18 +87,11 @@ cp -R YACReaderLibraryServer.app "${dest}/YACReaderLibraryServer"
 cp COPYING.txt "${dest}/"
 cp README.md "${dest}/"
 
-#mkdir -p "${dest}/icons/"
-#cp images/db.png "${dest}/icons/"
-#cp images/coversPackage.png "${dest}/icons/"
-
 echo "Creating dmg package"
-#tar -czf "${dest}".tar.gz "${dest}"
-#hdiutil create "${dest}".dmg -srcfolder "./${dest}" -ov
-
-#create-dmg --volname "YACReader $VERSION.$BUILD_NUMBER Installer" --volicon icon.icns --window-size 600 403 --icon-size 128 --app-drop-link 485 90 --background background.png --icon YACReader 80 90 --icon YACReaderLibrary 235 90 --eula COPYING.txt --icon YACReaderLibraryServer 470 295 --icon README.md 120 295 --icon COPYING.txt 290 295 "$dest.dmg" "$dest"
 
 sed -i'' -e "s/#VERSION#/$VERSION/g" dmg.json
 sed -i'' -e "s/#BUILD_NUMBER#/$BUILD_NUMBER/g" dmg.json
+sed -i'' -e "s/#QT_VERSION#/$QT_VERSION/g" dmg.json
 appdmg dmg.json "$dest.dmg"
 
 if [ "$SKIP_CODESIGN" = false ]; then
