60413d9c9fe3dd0656b71f9948052967743c0cb8
Luis Ángel San Martín <luisangelsm@gmail.com>
2024-09-22 17:22:30 +0200

    Update the script for building in Ubuntu 24.04
    It builds now 7z.so with rar support, so no codecs are needed at runtime, YACReader no longer loads separated codecs.

--- build_scripts/ubuntu_24.04/build.sh
+++ build_scripts/ubuntu_24.04/build.sh
@@ -40,7 +40,7 @@ mkdir -p "$COMPRESSED_ARCHIVE_DIR"
 # Step 2: Install required packages
 echo "Updating package list and installing required packages..."
 sudo apt-get update
-sudo apt-get install -y qtchooser qt6-tools-dev qt6-base-dev-tools qmake6 qmake6-bin qt6-base-dev qt6-multimedia-dev qt6-tools-dev-tools libgl-dev qt6-l10n-tools libqt6opengl6-dev qt6-declarative-dev libqt6svg6-dev libqt6core5compat6-dev qml6-module-qt5compat-graphicaleffects qt6-5compat-dev  libpoppler-qt6-dev qt6-image-formats-plugins qml6-module-qtquick-controls qml6-module-qtquick-layouts qml6-module-qtqml-workerscript qml6-module-qtquick-templates p7zip-full p7zip-rar git build-essential
+sudo apt-get install -y qtchooser qt6-tools-dev qt6-base-dev-tools qmake6 qmake6-bin qt6-base-dev qt6-multimedia-dev qt6-tools-dev-tools libgl-dev qt6-l10n-tools libqt6opengl6-dev qt6-declarative-dev libqt6svg6-dev libqt6core5compat6-dev qml6-module-qt5compat-graphicaleffects qt6-5compat-dev  libpoppler-qt6-dev qt6-image-formats-plugins qml6-module-qtquick-controls qml6-module-qtquick-layouts qml6-module-qtqml-workerscript qml6-module-qtquick-templates 7zip-full 7zip-rar git build-essential
 
 # Step 3: Download YACReader source code
 if [ "$1" == "develop" ]; then
@@ -60,14 +60,21 @@ wget "$SEVENZIP_URL" -O "$SEVENZIP_ARCHIVE"
 echo "Extracting 7zip source code..."
 7z x "$SEVENZIP_ARCHIVE" -o"$SEVENZIP_DIR"
 
-# Step 5: Build YACReader
+# Step 5: Build and install 7z.so with RAR support
+echo "Building and installing 7z.so with RAR support..."
+cd "$SEVENZIP_DIR/CPP/7zip/Bundles/Format7zF"
+make -f makefile.gcc
+sudo mkdir -p /usr/lib/7zip
+sudo cp ./_o/7z.so /usr/lib/7zip
+
+# Step 6: Build YACReader
 echo "Building YACReader..."
 cd "$YACREADER_DIR"
 export DEFINES_VAR=DEFINES+\=\"BUILD_NUMBER=\\\\\\\"CUSTOM_BUILD\\\\\\\"\"
 qmake6 CONFIG+="7zip" $DEFINES_VAR
 make
 
-# Step 6: Install YACReader
+# Step 7: Install YACReader
 echo "Installing YACReader..."
 sudo make install
 
