8942e46aaa92197d2a95c27fe0b0a74bbc20a7e1
luisangelsm <luisangelsm@gmail.com>
2026-01-12 18:35:59 +0100

    Add ThemeManager
    It can listen to system changes and update the current theme, widgets can use it to get the current theme.

---
+++ common/themes/theme_manager.cpp
@@ -0,0 +1,50 @@
+#include "theme_manager.h"
+
+#include "theme.h"
+
+#include <QGuiApplication>
+#include <QStyleHints>
+
+ThemeManager::ThemeManager()
+{
+}
+
+ThemeManager &ThemeManager::instance()
+{
+    static ThemeManager instance;
+    return instance;
+}
+
+void ThemeManager::initialize()
+{
+    auto *styleHints = qGuiApp->styleHints();
+
+    auto colorScheme = styleHints->colorScheme();
+
+    // TODO: settings are needed to decide what theme to use
+    auto applyColorScheme = [this](Qt::ColorScheme scheme) {
+        setTheme(scheme == Qt::ColorScheme::Dark ? ThemeId::Dark : ThemeId::Light);
+    };
+
+    applyColorScheme(colorScheme);
+
+    connect(styleHints, &QStyleHints::colorSchemeChanged, this, applyColorScheme, Qt::QueuedConnection);
+}
+
+void ThemeManager::setTheme(ThemeId themeId)
+{
+    if (this->themeId == themeId) {
+        return;
+    }
+
+    this->themeId = themeId;
+
+    updateCurrentTheme();
+
+    emit themeChanged();
+}
+
+void ThemeManager::updateCurrentTheme()
+{
+    // TODO
+}
