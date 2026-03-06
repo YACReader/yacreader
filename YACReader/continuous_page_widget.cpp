ffb8540c4fdc79939c59cb96cf6515a17db12f22
luisangelsm <luisangelsm@gmail.com>
2026-03-06 17:18:14 +0100

    Use the new resize image API to resize the pages while reading

--- YACReader/continuous_page_widget.cpp
+++ YACReader/continuous_page_widget.cpp
@@ -1,6 +1,7 @@
 #include "continuous_page_widget.h"
 #include "continuous_view_model.h"
 #include "render.h"
+#include "resize_image.h"
 
 #include <QPainter>
 #include <QPaintEvent>
@@ -183,7 +184,8 @@ const QImage *ContinuousPageWidget::scaledImageForPaint(int pageIndex, const QIm
     entry.sourceCacheKey = sourceKey;
     entry.sourceSize = source->size();
     entry.targetSize = targetSize;
-    entry.scaledImage = source->scaled(targetSize, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+    entry.scaledImage = scaleImage(*source, targetSize.width(), targetSize.height(),
+                                   Configuration::getConfiguration().getScalingMethod());
     scaledPageCache.pages.insert(pageIndex, std::move(entry));
 
     return &scaledPageCache.pages[pageIndex].scaledImage;
